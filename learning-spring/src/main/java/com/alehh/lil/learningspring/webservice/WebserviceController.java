18e65e3b6dfea50747dc8eff6e85b460a2b819d0
Aleh Hutyrchyk <Aleh_Hutyrchyk@epam.com>
2023-06-30 11:59:52 +0200

    completed course

---
+++ learning-spring/src/main/java/com/alehh/lil/learningspring/webservice/WebserviceController.java
@@ -0,0 +1,45 @@
+package com.alehh.lil.learningspring.webservice;
+
+import com.alehh.lil.learningspring.business.ReservationService;
+import com.alehh.lil.learningspring.business.RoomReservation;
+import com.alehh.lil.learningspring.data.Guest;
+import com.alehh.lil.learningspring.data.Room;
+import com.alehh.lil.learningspring.util.DateUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class WebserviceController {
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public WebserviceController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservations(@RequestParam(value = "date", required = false)String dateString){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        return this.reservationService.getRoomReservationsForDate(date);
+    }
+
+    @GetMapping("/guests")
+    public List<Guest> getGuests(){
+        return this.reservationService.getHotelGuests();
+    }
+
+    @PostMapping("/guests")
+    public void addGuest(@RequestBody Guest guest){
+        this.reservationService.addGuest(guest);
+    }
+
+    @GetMapping("/rooms")
+    public List<Room> getRooms(){
+        return this.reservationService.getRooms();
+    }
+
+}
